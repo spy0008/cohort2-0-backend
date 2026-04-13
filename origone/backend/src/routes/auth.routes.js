6281256abf400044196ef1fc1ee5ca49ae9604b3
naman sompura <namansompura2@gmail.com>
2026-04-13 19:25:53 +0530

    feat: initialize backend with Express, MongoDB, and Google OAuth
    - Added package.json for backend dependencies and scripts
    - Created server.js to set up the Express server and connect to MongoDB
    - Implemented app.js with middleware for logging, parsing, and authentication
    - Configured environment variables in config.js and established MongoDB connection in db.js
    - Developed user model with password hashing and comparison methods
    - Created authentication controller for user registration, login, and Google OAuth callback
    - Set up routes for authentication with validation middleware
    - Added request validation for user registration and login
    - Included .gitignore for frontend to exclude logs and environment files

---
+++ origone/backend/src/routes/auth.routes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { validateRegisterUser, validateLoginUser } from "../validator/auth.validator.js";
+import { googleCallback, login, register } from "../controllers/auth.controller.js";
+import passport from "passport";
+import { config } from "../config/config.js";
+
+const router = Router();
+
+
+
+router.post('/register', validateRegisterUser, register)
+
+router.post("/login", validateLoginUser, login)
+
+
+// /api/auth/google
+router.get("/google",
+    passport.authenticate("google", { scope: [ "profile", "email" ] }))
+
+router.get("/google/callback",
+    passport.authenticate("google", {
+        session: false,
+        failureRedirect: config.NODE_ENV == "development" ? "http://localhost:5173/login" : "/login"
+    }),
+    googleCallback,
+)
+
+export default router;

86d353a77f6299eb964382782a4995ce77f64591
naman sompura <namansompura2@gmail.com>
2026-04-16 19:30:06 +0530

    feat: enhance authentication and product management features

--- origone/backend/src/controllers/product.controller.js
+++ origone/backend/src/controllers/product.controller.js
@@ -1,48 +1,55 @@
 import productModel from "../models/product.model.js";
 import { uploadFile } from "../services/storage.service.js";
 
-
 export async function createProduct(req, res) {
+  const { title, description, priceAmount, priceCurrency } = req.body;
+  const seller = req.user;
 
-    const { title, description, priceAmount, priceCurrency } = req.body;
-    const seller = req.user;
+  const images = await Promise.all(
+    req.files.map(async (file) => {
+      return await uploadFile({
+        buffer: file.buffer,
+        fileName: file.originalname,
+      });
+    }),
+  );
 
-    const images = await Promise.all(req.files.map(async (file) => {
-        return await uploadFile({
-            buffer: file.buffer,
-            fileName: file.originalname
-        })
-    }))
+  const product = await productModel.create({
+    title,
+    description,
+    price: {
+      amount: priceAmount,
+      currency: priceCurrency || "INR",
+    },
+    images,
+    seller: seller._id,
+  });
 
-
-    const product = await productModel.create({
-        title,
-        description,
-        price: {
-            amount: priceAmount,
-            currency: priceCurrency || "INR"
-        },
-        images,
-        seller: seller._id
-    })
-
-
-    res.status(201).json({
-        message: "Product created successfully",
-        success: true,
-        product
-    })
+  res.status(201).json({
+    message: "Product created successfully",
+    success: true,
+    product,
+  });
 }
 
 export async function getSellerProducts(req, res) {
-    const seller = req.user;
+  const seller = req.user;
 
-    const products = await productModel.find({ seller: seller._id });
+  const products = await productModel.find({ seller: seller._id });
 
+  res.status(200).json({
+    message: "Products fetched successfully",
+    success: true,
+    products,
+  });
+}
 
-    res.status(200).json({
-        message: "Products fetched successfully",
-        success: true,
-        products
-    })
+export async function getProducts(req, res) {
+  const products = await productModel.find();
+
+  res.status(200).json({
+    message: "Products fetched successfully",
+    success: true,
+    products,
+  });
 }
--- origone/backend/src/routes/product.routes.js
+++ origone/backend/src/routes/product.routes.js
@@ -1,36 +1,47 @@
-import express from 'express';
-import { authenticateSeller } from '../middlewares/auth.middleware.js';
-import { createProduct, getSellerProducts } from '../controllers/product.controller.js';
+import express from "express";
+import { authenticateSeller } from "../middlewares/auth.middleware.js";
+import {
+  createProduct,
+  getProducts,
+  getSellerProducts,
+} from "../controllers/product.controller.js";
 import multer from "multer";
-import { createProductValidator } from '../validator/product.validator.js';
-
+import { createProductValidator } from "../validator/product.validator.js";
 
 const upload = multer({
-    storage: multer.memoryStorage(),
-    limits: {
-        fileSize: 5 * 1024 * 1024 // 5 MB
-    }
-})
-
-
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
+  },
+});
 
 const router = express.Router();
-
 
 /**
  * @route POST /api/products
  * @description Create a new product
  * @access Private (Seller only)
  */
-router.post("/", authenticateSeller, upload.array('images', 7), createProductValidator, createProduct)
-
+router.post(
+  "/",
+  authenticateSeller,
+  upload.array("images", 7),
+  createProductValidator,
+  createProduct,
+);
 
 /**
  * @route GET /api/products/seller
  * @description Get all products of the authenticated seller
  * @access Private (Seller only)
  */
-router.get("/seller", authenticateSeller, getSellerProducts)
+router.get("/seller", authenticateSeller, getSellerProducts);
 
+/**
+ * @route GET /api/products
+ * @description Get all products
+ * @access Public
+ */
+router.get("/", getProducts);
 
 export default router;
